9ded7eecd69d20357c6517a44c106352bd84a748
Angel Garcia <angelgarciaweb@gmail.com>
2022-07-11 09:46:17 -0400

    Terminar la base de las variables

--- variables.py
+++ variables.py
@@ -2,4 +2,11 @@
 name = "Angel"
 lastname = "Garcia"
 
+x, y = 10, 20
+
 print( name + " " + lastname )
+print( x, y )
+
+# CONSTANTES
+PI = 3.1416 # Es lo mismo que una variable, pero para los programadores es identificar cuales no se deben cambiar
+MI_NAME = 'Angel'
