05b24202d3eb0fabca3500cefbaa7cd4e7539353
Angel Garcia <angelgarciaweb@gmail.com>
2022-07-11 11:48:01 -0400

    Lists

---
+++ lists.py
@@ -0,0 +1,40 @@
+
+
+demo_list = [1, 'hello', 1.32, True, [1, 2, 3]]
+colors = ['red', 'green', 'blue']
+
+number_list = list((1, 2, 3, 4))
+
+# print(number_list)
+
+# r = list(range(1, 10)) # Obtener un rango del 1 al 9
+
+# print(colors[1])
+# print('green' in colors)
+
+# print(colors)
+
+# colors[1] = 'yellow'
+
+# print(colors)
+
+
+# colors.append('violet')
+# colors.extend(('violet', 'yellow')) # Extiende en base a listas o a tuplas
+# colors.extend(['pink', 'black'])
+
+
+# colors.insert(-1, 'violet')
+# colors.insert(len(colors), 'violet')
+# print(colors)
+
+# colors.pop(1) # Elimina un elemento a traves de su indice y si no se coloca el indice se elimina el ultiimo
+
+# colors.remove('green') # Elimina un elemento en especifico
+
+# colors.clear() # Deja la lista vacia
+
+colors.sort() # Ordenar alfabeticamente
+colors.sort(reverse=True) # Ordena de manera alfabetica alreves
+
+print(colors)
