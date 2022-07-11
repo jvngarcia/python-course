7c655e348ea0b551116afebe6e4d8de0d5848306
Angel Garcia <angelgarciaweb@gmail.com>
2022-07-11 10:22:20 -0400

    Strings

---
+++ strings.py
@@ -0,0 +1,19 @@
+my_str = 'Angel'
+
+print('My name is ' + my_str)
+print(f'My name is {my_str}')
+print('My name is {0}'.format(my_str))
+
+# print(dir(my_str))
+# print(my_str.replace('Hello', 'Bye'))
+# print(my_str.count('l'))
+# print(my_str.startswith('Hello'))
+# print(my_str.endswith('ld'))
+# print(my_str.split(', '))
+# print(my_str.find('o'))
+# print(len(my_str))
+# print(my_str.index('e'))
+# print(my_str.isnumeric())
+# print(my_str.isalpha())
+# print(my_str[4])
+# print(my_str[-2])
