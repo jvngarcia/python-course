1105ad2382f4e32cc41fdc22170898fd6eace2d9
Angel Garcia <angelgarciaweb@gmail.com>
2022-07-11 11:02:35 -0400

    Numbers

---
+++ numbers.py
@@ -0,0 +1,14 @@
+
+
+# print(1+1)
+# print(2 ** 3) # Numero elevado
+# print(2 // 2) # Devolver el numero sin decimal
+# print( 2 % 2 ) # Devuelve el residuo
+
+
+age = input('Insert your age: ')
+
+new_age = int(age) + 5
+float_age = float(age) + 4
+
+print( new_age )
